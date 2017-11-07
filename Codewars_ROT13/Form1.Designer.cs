b86c8cfb2bfcb6c1ccc6aecc1f71f2fcfaa06206
Archangel37 <djarchangel3737@gmail.com>
2017-11-07 14:21:24 +0300

    solving ROT13 on Codewars.com

---
+++ Codewars_ROT13/Form1.Designer.cs
@@ -0,0 +1,83 @@
+﻿namespace Codewars_ROT13
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.richTextBox_input = new System.Windows.Forms.RichTextBox();
+            this.richTextBox_output = new System.Windows.Forms.RichTextBox();
+            this.button_convert = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // richTextBox_input
+            // 
+            this.richTextBox_input.Location = new System.Drawing.Point(12, 41);
+            this.richTextBox_input.Name = "richTextBox_input";
+            this.richTextBox_input.Size = new System.Drawing.Size(631, 173);
+            this.richTextBox_input.TabIndex = 0;
+            this.richTextBox_input.Text = "";
+            // 
+            // richTextBox_output
+            // 
+            this.richTextBox_output.Location = new System.Drawing.Point(12, 231);
+            this.richTextBox_output.Name = "richTextBox_output";
+            this.richTextBox_output.Size = new System.Drawing.Size(631, 173);
+            this.richTextBox_output.TabIndex = 0;
+            this.richTextBox_output.Text = "";
+            // 
+            // button_convert
+            // 
+            this.button_convert.Location = new System.Drawing.Point(284, 12);
+            this.button_convert.Name = "button_convert";
+            this.button_convert.Size = new System.Drawing.Size(75, 23);
+            this.button_convert.TabIndex = 1;
+            this.button_convert.Text = "Convert";
+            this.button_convert.UseVisualStyleBackColor = true;
+            this.button_convert.Click += new System.EventHandler(this.button_convert_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(679, 447);
+            this.Controls.Add(this.button_convert);
+            this.Controls.Add(this.richTextBox_output);
+            this.Controls.Add(this.richTextBox_input);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox richTextBox_input;
+        private System.Windows.Forms.RichTextBox richTextBox_output;
+        private System.Windows.Forms.Button button_convert;
+    }
+}
+
---
+++ Codewars_ROT13/Form1.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
+
+
+//ROT13
+
+//How can you tell an extrovert from an introvert at NSA? Va gur ryringbef, gur rkgebireg ybbxf ng gur BGURE thl'f fubrf.
+
+//I found this joke on USENET, but the punchline is scrambled.Maybe you can decipher it? According to Wikipedia, ROT13 (http://en.wikipedia.org/wiki/ROT13) is frequently used to obfuscate jokes on USENET.
+
+//Hint: For this task you're only supposed to substitue characters. Not spaces, punctuation, numbers etc. Test examples:
+
+//rot13("EBG13 rknzcyr.") == "ROT13 example.";
+//rot13("This is my first ROT13 excercise!" == "Guvf vf zl svefg EBG13 rkprepvfr!"
+
+//best solution:
+//using System;
+//using System.Linq;
+//public class Kata
+//{
+//    public static string Rot13(string input)
+//    {
+//        var s1 = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+//        var s2 = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+//        return string.Join("", input.Select(x => char.IsLetter(x) ? s1[s2.IndexOf(x)] : x));
+//    }
+//}
+
+
+namespace Codewars_ROT13
+{
+    public partial class Form1 : Form
+    {
+        public List<char> high1 = new List<char> {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M'};
+        public List<char> high2 = new List<char> {'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'};
+        public List<char> low1 = new List<char> {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm'};
+        public List<char> low2 = new List<char> {'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        public string rot13(string input)
+        {
+            var ch_input = new List<char>();
+            for (var i = 0; i < input.Length; i++)
+                ch_input.Add(input[i]);
+            var result = string.Empty;
+
+            for (var n = 0; n < ch_input.Count; n++)
+                if (high1.Contains(ch_input[n]))
+                    result += high2[high1.IndexOf(ch_input[n])];
+                else if (high2.Contains(ch_input[n]))
+                    result += high1[high2.IndexOf(ch_input[n])];
+                else if (low1.Contains(ch_input[n]))
+                    result += low2[low1.IndexOf(ch_input[n])];
+                else if (low2.Contains(ch_input[n]))
+                    result += low1[low2.IndexOf(ch_input[n])];
+                else
+                    result += ch_input[n];
+            return result;
+        }
+
+
+        private void button_convert_Click(object sender, EventArgs e)
+        {
+            richTextBox_output.Text = rot13(richTextBox_input.Text);
+        }
+    }
+}
